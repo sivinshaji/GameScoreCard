489664dbcd6160e9bbe7ee0da61e39540c45a594
Sivin <sivin9@gmail.com>
2022-10-24 15:31:44 +0400

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -49,27 +49,19 @@ function Blackjack(state)
 {
     GameState = state
     playerbetpoints = 10
-    combetpoints = 10
     document.getElementById('bet-value').innerText = playerbetpoints
     document.getElementById('current-value').innerText = totalPlayerPoint
     if(GameState)
     {
         GameState = false
         PlayerCards = []
-        ComCards = []
         sumPlayerCard = 0;
-        sumComCard = 0;
         // First two card selection
         PlayerCards.push(randomCardSelector())
         PlayerCards.push(randomCardSelector())
-        ComCards.push(randomCardSelector())
-        ComCards.push(randomCardSelector())
         //sum of first two cards
         PlayerCards.forEach(n => {
             sumPlayerCard += n
-        })
-        ComCards.forEach(n => {
-            sumComCard += n
         })
         //check initial blackjack
         checkBlackjack()
@@ -94,25 +86,19 @@ function randomCardSelector()
 function drawCard()
 {
     PlayerCards.push(randomCardSelector())
-    ComCards.push(randomCardSelector())
         //sum cards with new cards
         sumPlayerCard = 0
-        sumComCard = 0
         PlayerCards.forEach(n => {
             sumPlayerCard += n
         })
-        ComCards.forEach(n => {
-            sumComCard += n
-        })
         updateMessage()
-        console.log(`My Card Value: ${sumPlayerCard} Com Card Value: ${sumComCard}`)
+        console.log(`My Card Value: ${sumPlayerCard}`)
 }
 
 //Rais bet value
 function raiseBet()
 {
     playerbetpoints += 10
-    combetpoints += 10
     document.getElementById('bet-value').innerText = `${playerbetpoints}`
 }
 
@@ -126,36 +112,13 @@ function updateMessage()
 //Check Blackjack
 function checkBlackjack()
 {
-    
-    if(sumPlayerCard === 21 && sumComCard === 21)
+    if(sumPlayerCard === 21)
     {
-        document.getElementById('draw-cards').innerText = "Draw"
-        updateMessage()
-        btn = document.getElementsByClassName('btn-place-bet')[0]
-        btn.classList.add("hidden")
-        btn = document.getElementsByClassName('btn-pass-chance')[0]
-        btn.classList.add("hidden")
-        btn = document.getElementsByClassName('btn-drop-game')[0]
-        btn.classList.add("hidden")
-        btn = document.getElementsByClassName('btn-startgame')[0]
-        btn.classList.remove("hidden")
-    }
-    else if(sumPlayerCard === 21 || sumComCard === 21)
-    {
-        if (sumPlayerCard === 21)
-        {
-            totalPlayerPoint = totalPlayerPoint + playerbetpoints + combetpoints
-            totalComPoint = totalComPoint - combetpoints;
-            document.getElementById('bet-value').innerText = playerbetpoints
-            document.getElementById('current-value').innerText = totalPlayerPoint
-        }
-        else
-        {
-            totalComPoint = totalComPoint + playerbetpoints + combetpoints
-            totalPlayerPoint = totalPlayerPoint - playerbetpoints;
-            document.getElementById('bet-value').innerText = playerbetpoints
-            document.getElementById('current-value').innerText = totalPlayerPoint
-        }
+        
+        totalPlayerPoint = totalPlayerPoint + playerbetpoints
+        document.getElementById('bet-value').innerText = playerbetpoints
+        document.getElementById('current-value').innerText = totalPlayerPoint
+
         document.getElementById('draw-cards').innerText = "B L A C K j A C K"
         updateMessage()
         btn = document.getElementsByClassName('btn-place-bet')[0]
@@ -167,27 +130,10 @@ function checkBlackjack()
         btn = document.getElementsByClassName('btn-startgame')[0]
         btn.classList.remove("hidden")
     }
-    else if(sumComCard > 21)
-    {
-        document.getElementById('draw-cards').innerText = `B L A C K j A C K\nPlayer Wins`
-        updateMessage()
-        btn = document.getElementsByClassName('btn-place-bet')[0]
-        btn.classList.add("hidden")
-        btn = document.getElementsByClassName('btn-pass-chance')[0]
-        btn.classList.add("hidden")
-        btn = document.getElementsByClassName('btn-drop-game')[0]
-        btn.classList.add("hidden")
-        btn = document.getElementsByClassName('btn-startgame')[0]
-        btn.classList.remove("hidden")
-        totalPlayerPoint = totalPlayerPoint + playerbetpoints + combetpoints
-        totalComPoint = totalComPoint - combetpoints;
-        document.getElementById('bet-value').innerText = playerbetpoints
-        document.getElementById('current-value').innerText = totalPlayerPoint
-
-    }
+    
     else if(sumPlayerCard > 21)
     {
-        document.getElementById('draw-cards').innerText = `B L A C K j A C K\nCOM Wins`
+        document.getElementById('draw-cards').innerText = `You Lose`
         updateMessage()
         btn = document.getElementsByClassName('btn-place-bet')[0]
         btn.classList.add("hidden")
@@ -197,7 +143,6 @@ function checkBlackjack()
         btn.classList.add("hidden")
         btn = document.getElementsByClassName('btn-startgame')[0]
         btn.classList.remove("hidden")
-        totalComPoint = totalComPoint + playerbetpoints + combetpoints
         totalPlayerPoint = totalPlayerPoint - playerbetpoints;
         document.getElementById('bet-value').innerText = playerbetpoints
         document.getElementById('current-value').innerText = totalPlayerPoint
@@ -214,6 +159,9 @@ function checkBlackjack()
         btn.classList.remove("hidden")
         btn = document.getElementsByClassName('btn-startgame')[0]
         btn.classList.add("hidden")
+        
+        document.getElementById('bet-value').innerText = playerbetpoints
+        document.getElementById('current-value').innerText = totalPlayerPoint
     }
-    console.log(`My Card Value: ${sumPlayerCard} Com Card Value: ${sumComCard}`)
+    
 }
